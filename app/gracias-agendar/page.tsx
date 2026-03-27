a445fa00f44cc857969202fdfc817aef2225db01
Prado2209 <nelson.prado@cun.edu.co>
2026-03-27 11:44:05 -0500

    11:43 add footer and navbar

--- app/gracias-agendar/page.tsx
+++ app/gracias-agendar/page.tsx
@@ -1,14 +1,29 @@
+"use client";
+
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
 export default function GraciasAgendar() {
-    return (
-        <div style={{ padding: "40px" }}>
-            <h1>Agenda tu reunión</h1>
-            
-            <iframe
+    return (      
+<main className="min-h-screen flex flex-col bg-white text-black">
+      {/* Navbar */}
+      <Navbar />
+
+      {/* Contenido principal */}
+      <section className="flex-1 flex flex-col items-center justify-center px-6 py-20 text-center">
+        <h1 className="text-4xl md:text-5xl font-semibold mb-4">
+          Agenda tu reunión
+        </h1>
+        <iframe
              src="https://calendly.com/nelsonpc1549/30min" 
              width="100%"
              height="600"
              />
-        
-        </div>
+      </section>
+
+      {/* Footer */}
+      <Footer />
+    </main>
+
     );
 }
