943f27cdad7f89d0f9f2cfb0847f4e8452cce8f8
Prado2209 <nelson.prado@cun.edu.co>
2026-04-11 08:20:28 -0500

    Fix contact route

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,9 @@
 import type { NextConfig } from "next";
-import { ignoreListAnonymousStackFramesIfSandwiched } from "next/dist/next-devtools/server/shared";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
-
-module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
   },
 };
+
+export default nextConfig;
